f2389c4c047a16145861aee75ead7c5730a02522
Okkon <2214733006@qq.com>
2023-05-08 21:43:15 +0800

    2023.5.8

---
+++ lab1/Collatz.java
@@ -0,0 +1,30 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
+public class Collatz {
+
+    /** if n is even,return n/2.
+     *  if n is odd,return 3*n+1
+     *  */
+    public static int nextNumber(int n) {
+        if(n%2==0)
+        {
+            return n/2;
+        }
+        else
+        {
+            return 3*n+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
