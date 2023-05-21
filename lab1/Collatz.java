655d0f54a9120168244856af07a0b32d5acd1745
Okkon <2214733006@qq.com>
2023-05-21 09:41:29 +0800

    2023.5.21 lab4A

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -6,14 +6,14 @@ public class Collatz {
     /** if n is even,return n/2.
      *  if n is odd,return 3*n+1
      *  */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if(n%2==0)
-        {
-            return n/2;
-        }
-        else
-        {
-            return 3*n+1;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
